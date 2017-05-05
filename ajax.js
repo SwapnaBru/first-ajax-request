c2274483c2291cd0b4a246af4dfcb1f36be64816
Swapna Brungi <swapna.brungi@gmail.com>
2017-05-04 23:39:05 -0400

    created the request for step7 till step 9

--- ajax.js
+++ ajax.js
@@ -1,24 +1,16 @@
 $(function () {
-// $('').on('click', function(){
-//   console.log('Before.ajax');
+$('#ajax_request').on('click', function(){
+  console.log('Before.ajax');
 
   /* Your code goes here */
   $.ajax({
     url: 'http://first-ajax-api.herokuapp.com/',
     method: 'GET',
-    data: 'text',
-        // dataType: _____
+        dataType: 'text'
       }).done(function(data) {
-            console.log('.ajax done')
-            $('body').append(data);
-          }).fail(function(jqXHR, textStatus) {
-            console.log('.ajax FAILED with ' + jqXHR.status + ' ' + jqXHR.statusText);
-            console.log(jqXHR);
-          }).always(function() {
-            console.log('.ajax request finished');
-          });
+            console.log('.ajax done');
+          })
 
-          console.log('After .ajax');
         });
 
         $('#step3456').on('click', function(){
@@ -26,12 +18,60 @@ $(function () {
             url:'http://first-ajax-api.herokuapp.com/ping',
             method: 'GET',
             dataType: 'text'
-          }).done(function(){
+          }).done(function(responseData){
             console.log('.ajax done')
-            $('#step3456').append('<p>' + data + '</p>');
+            $('#step3456').append(responseData);
           }).fail(function(){
-            $('#step3456').append('<p> ERROR: Could not connect to Pond </p>');
+            $('#step3456').append('ERROR: Could not load to Pong');
           }).always(function(){
             console.log('.ajax request finished')
           });
+        });
+
+        $('#step7').on('click', fucntion(){
+          $ajax({
+            url:'http://first-ajax-api.herokuapp.com/count',
+            method: 'GET',
+            dataType: 'text'
+          }).done(function(responseData){
+            $('#step7').append(responseData);
+          });
+        });
+
+        $('#step8').on('click', function(){
+          $ajax({
+            url:'http://first-ajax-api.herokuapp.com/time',
+            method: 'GET',
+            data:{ timezone: 'Pacific/Honolulu'},
+            dataType: 'text'
+          }).done(function(responseData){
+            $('#step8').append('<p>' + responseData + '</p>');
+        });
+        });
+
+        $('#step9').on('click', function(){
+          $.ajax({
+            url: 'http://first-ajax-api.herokuapp.com/a_car',
+            method: 'GET',
+            dataType: 'html'
+          }).done(function(responseData){
+            $('#a_car').append(responseData);
+          });
+        });
+
+
+
+      });
+
+
+
+
+
+
+
+
+
+
+
+            console.log('After .ajax');
         });
